d54fc7f18f832bab9dc1e1598712e96c284ec1a7
Arifin Firdaus <arifin.firdaus@icehousecorp.com>
2021-10-05 13:30:22 +0700

    Initial Commit

---
+++ UsersApp/UsersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersAppApp.swift
+//  UsersApp
+//
+//  Created by Arifin Firdaus on 05/10/21.
+//
+
+import SwiftUI
+
+@main
+struct UsersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UsersAppUITests/UsersAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UsersAppUITestsLaunchTests.swift
+//  UsersAppUITests
+//
+//  Created by Arifin Firdaus on 05/10/21.
+//
+
+import XCTest
+
+class UsersAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
